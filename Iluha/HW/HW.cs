acf48a9be0789b8f8a9e05304bebfa161f64906f
koshmanovia <k0shmanov@yandex.ru>
2017-07-09 21:24:45 +0300

    test VS link

---
+++ Iluha/HW/HW.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HW
+{
+    class HW
+    {
+         public static void Main(String[] args)
+        {
+                Console.WriteLine("GIVE MY MONEY BUTCH!");
+                Console.ReadKey();
+        }
+    }
+}
