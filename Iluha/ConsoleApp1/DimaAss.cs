2202d474035832407ab1b5edcf7a7e035ff960ad
koshmanovia <k0shmanov@yandex.ru>
2017-07-09 22:57:16 +0300

    Test Pidora Dimk

---
+++ Iluha/ConsoleApp1/DimaAss.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace ConsoleApp1
+{
+    public class DimaAss
+    {       
+        public static void RunFuck() {
+            String assNew = "*";
+            String assOld = "O";
+            String name = "   <--- Димкины Булочки";
+             String[] fuckDimaAss = new String[] { "8===D (_" + assNew + "_)"+name, " 8===D(_" + assNew + "_)"+name, "  8===D_" + assNew + "_)"+name, "   8===D" + assNew + "_)"+name, "    8===D_)"+name, "     8==_)"+name, "      8=_)"+name, "       8_)"+name } ;
+             String[] fuckDimaAss2 = new String[] { "8===D (_" + assOld + "_)"+name, " 8===D(_" + assOld + "_)"+name, "  8===D_" + assOld + "_)"+name, "   8===D" + assOld + "_)"+name } ;
+             for (int i = 0; i < fuckDimaAss.Length; i++)
+            {
+            Console.WriteLine(fuckDimaAss[i]);
+            Thread.Sleep(500);
+            Console.Clear();
+            }
+
+             for (int i = 0; i < fuckDimaAss2.Length; i++){ fuckDimaAss[i] = fuckDimaAss2[i];}
+
+             for (int i = fuckDimaAss.Length - 1; i > 0 ; i--)
+            {
+            Console.WriteLine(fuckDimaAss[i]);
+            Thread.Sleep(500);
+            Console.Clear();
+            }
+            
+        }
+        
+    
+    }
+}
---
+++ Iluha/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp;
+            do{
+                Console.Clear();
+                Console.WriteLine("Пидор Димка или нет?");
+                Console.WriteLine("1. Димка Пидор, я согласен");
+                Console.WriteLine("2. Нужны доказательства");
+                inp = Console.ReadLine().ToString();
+
+            if (inp != "1")
+                {
+                    Console.Clear();
+                    DimaAss Zadv = new DimaAss();
+                    DimaAss.RunFuck();
+                    Console.WriteLine("Доказательства на лицо, Димка Пидор!");
+                    inp = Console.ReadLine().ToString();
+                    
+                }
+            
+            }  while (inp != "1");        
+        }
+    }
+}
