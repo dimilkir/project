83c84ca083ffe7c19fafdbc18069b4872162c864
kirkenbro <iberezk7@gmail.com>
2017-07-13 22:56:20 +0300

    upd2

--- kirken/WFA1/WFA1/Form1.Designer.cs
+++ kirken/WFA1/WFA1/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.Yellow;
+            this.button1.ForeColor = System.Drawing.SystemColors.InfoText;
             this.button1.Location = new System.Drawing.Point(56, 44);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 128);
@@ -51,7 +51,7 @@
             this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Name = "mainForm";
-            this.Text = "жми чаще";
+            this.Text = "Гипноформа";
             this.ResumeLayout(false);
 
         }
--- kirken/WFA1/WFA1/Form1.cs
+++ kirken/WFA1/WFA1/Form1.cs
@@ -19,20 +19,27 @@ namespace WFA1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            for(int c=0; c<=253; c++)
+            while (true)
             {
-                this.BackColor = Color.FromArgb(c, 255 - c, c);
-                button1.BackColor = Color.FromArgb(255 - c, c, 255 - c);
-                Application.DoEvents();
-            //    System.Threading.Thread.Sleep(3);
-            }
 
-            for (int c = 254; c >=0; c--)
-            {
-                this.BackColor = Color.FromArgb(c, 255 - c, c);
-                button1.BackColor = Color.FromArgb(255 - c, c, 255 - c);
-                Application.DoEvents();
-              //  System.Threading.Thread.Sleep(3);
+                button1.Text = "ОТДАЙ ДУШУ!";
+                button1.ForeColor = Color.Yellow;
+
+                for (int c = 0; c <= 253; c++)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    button1.BackColor = Color.FromArgb(255 - c, c, 255 - c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
+
+                for (int c = 254; c >= 0; c--)
+                {
+                    this.BackColor = Color.FromArgb(c, 255 - c, c);
+                    button1.BackColor = Color.FromArgb(255 - c, c, 255 - c);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(3);
+                }
             }
         }
     }
