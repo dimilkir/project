2616fa90e8a3d22b25a028554159ec3814ee3f8b
kirkenbro <iberezk7@gmail.com>
2017-07-14 23:57:39 +0300

    glava 3

---
+++ kirken/App3/App3/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace App3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textToSay = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOfTimes = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfTimes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textToSay
+            // 
+            this.textToSay.Location = new System.Drawing.Point(72, 13);
+            this.textToSay.Name = "textToSay";
+            this.textToSay.Size = new System.Drawing.Size(200, 20);
+            this.textToSay.TabIndex = 0;
+            this.textToSay.Text = "Превед, медвед!";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Текст:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // numberOfTimes
+            // 
+            this.numberOfTimes.Location = new System.Drawing.Point(72, 39);
+            this.numberOfTimes.Name = "numberOfTimes";
+            this.numberOfTimes.Size = new System.Drawing.Size(200, 20);
+            this.numberOfTimes.TabIndex = 2;
+            this.numberOfTimes.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Счетчик:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(260, 35);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Жахнуть!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 111);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberOfTimes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textToSay);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Говорун";
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfTimes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textToSay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numberOfTimes;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
