3020739143e5b16d282fd864af6d877f6cbbb40c
kirkenbro <iberezk7@gmail.com>
2017-07-23 18:10:54 +0300

    Бананкулятор v.1

---
+++ kirken/App4/App4/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace App4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.finish = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(152, 13);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(120, 20);
+            this.start.TabIndex = 0;
+            this.start.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Бананов было:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(13, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Бананов стало:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // finish
+            // 
+            this.finish.Location = new System.Drawing.Point(152, 40);
+            this.finish.Name = "finish";
+            this.finish.Size = new System.Drawing.Size(120, 20);
+            this.finish.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(13, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Бананов накидано на:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.resultLabel.Location = new System.Drawing.Point(152, 67);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(120, 20);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.Text = "0 руб.";
+            this.resultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(259, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Прочекать бананы";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 129);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.finish);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.start);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Бананкулятор";
+            ((System.ComponentModel.ISupportInitialize)(this.start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.finish)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown finish;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ kirken/App4/App4/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App4
+{
+    public partial class Form1 : Form
+    {
+        int begin;
+        int end;
+
+        double nakidano;
+        double bananaPrice = 8.87; //средняя цена одного банана при среднем весе 150 грамм
+        double money;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            begin = (int)start.Value;
+            end = (int)finish.Value;
+
+            if (end > begin)
+            {
+                MessageBox.Show("Нех пиздеть мне тут!", "Э, йоба!");
+            }
+            else
+            {
+                nakidano = begin - end;
+                money = nakidano * bananaPrice;
+                resultLabel.Text = money + " руб.";
+            }
+        }
+    }
+}
